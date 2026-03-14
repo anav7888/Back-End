64d5647a5056559ef6016b90f8a4d846d4eae9c9
Bhuvneshwar <anavanav2222@gmail.com>
2026-03-14 17:45:06 +0530

    update

--- Moodify/Frontend/src/App.jsx
+++ Moodify/Frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import FaceExpression from './features/Expression/components/FaceExpression'
+import { RouterProvider } from "react-router"
+import {router} from "./app.routes"
+import "./features/shared/styles/global.scss"
 
-const App = () => {
+function App (){
   return (
-    <FaceExpression/>
+    <RouterProvider router={router} />
   )
 }
 
-export default App
+export default App 
