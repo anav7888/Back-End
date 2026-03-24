e12192795355d5dadc4fb154a9eb303a9701efe7
Bhuvneshwar <anavanav2222@gmail.com>
2026-03-25 02:12:52 +0530

    perplexity

---
+++ Perplexity/server.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import app from "./src/app.js";
+import connectDB from "./src/config/database.js";
+
+const PORT = process.env.PORT || 3000
+
+
+connectDB()
+    .catch((err)=>{
+        console.error("MONGO connection failed:", err);
+        process.exit(1);
+    });
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`);
+});
---
+++ Perplexity/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "server is running"
+    })
+
+})
+
+
+export default app
