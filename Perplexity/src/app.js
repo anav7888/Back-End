40a0cdb1dda3a50992fa262825f807affa42e839
Bhuvneshwar <anavanav2222@gmail.com>
2026-03-26 01:17:45 +0530

    nodemailer

--- Perplexity/src/app.js
+++ Perplexity/src/app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 })
 
-app.use("api/auth", authRouter)
+app.use("/api/auth", authRouter)
 
 
 export default app
--- Perplexity/src/controllers/auth.controller.js
+++ Perplexity/src/controllers/auth.controller.js
@@ -27,13 +27,21 @@ export async function register(req, res) {
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+               
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
     })
 
-
+        res.status(201).json({
+            message:"User register successfully",
+            success: true,
+            user:{
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        })
 
 }
 
