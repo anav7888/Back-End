522ea2071c7f6eeea821c4c2e47128f8a8281f76
Bhuvneshwar <anavanav2222@gmail.com>
2026-05-13 15:35:17 +0530

    mail

--- GenAI/index.js
+++ GenAI/index.js
@@ -1,7 +1,22 @@
 import "dotenv/config"
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage, tool, createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+
+const emailTool = tool(
+    sendEmail,
+    {
+        name: "emailTool",
+        description: "Use this tool to send an email",
+        schema: z.object({
+            to: z.string().description("the recipient email address"),
+            html: z.string().description("The html content of the email"),
+            subject: z.string().description("The subject of the email"),
+        })
+    }
+)
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,6 +27,11 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest"
 })
 
+const agent = createAgent({
+    model,
+    tools: [emailTool]
+})
+
 const message = []
 
 while (true) {
@@ -20,15 +40,15 @@ while (true) {
 
     message.push(new HumanMessage(userInput))
 
-    const response = await model.invoke(message)
+    const response = await agent.invoke(message)
 
     message.push(response)
 
-    console.log(`\x1b[34m[AI]\x1b[0m ${response.content}`)
+    console.log(response);
+
+    // console.log(`\x1b[34m[AI]\x1b[0m ${response.content}`)
 }
 
 
 rl.close();
 
-
-//gmail add
